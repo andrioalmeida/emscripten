413ec7f7c06a857fb0266d0a7343a03a161ae4b0
Sam Clegg <sbc@chromium.org>
2020-01-29 18:15:35 -0800

    feedback

--- tests/test_other.py
+++ tests/test_other.py
@@ -8727,10 +8727,11 @@ end
       self.assertContained('libfoo.a: archive has no index; run ranlib to add one', stderr)
     # The default behavior is to add archive indexes automatically.
     run_process([PYTHON, EMCC, 'libfoo.a', 'hello_world.o'])
-    run_process([PYTHON, EMCC, 'libfoo.a', 'hello_world.o'])
 
   def test_archive_non_objects(self):
     create_test_file('file.txt', 'test file')
+    # llvm-nm has issues with files that start with two or more null bytes since it thinks they
+    # are COFF files.  Ensure that we correctly ignore such files when we process them.
     create_test_file('zeros.bin', '\0\0\0\0')
     run_process([PYTHON, EMCC, '-c', path_from_root('tests', 'hello_world.c')])
     # No index added.
